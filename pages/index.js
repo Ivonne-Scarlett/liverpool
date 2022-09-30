63cb1182e8c21103b2a0ee6a647009384d47a322
Ivonne <ivonne.scarlett01@gmail.com>
2022-09-29 20:29:49 -0500

    Limpieza de next, agregar tailwind, colores y tipografía

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-pinkLiverpool">
-      ...
-      </h1>
-
-      <h1 className="underline text-grayLight">
-        Hello world!
-      </h1>
-
-      <p className="text-2xl font-bold">
-        Paga con tu tarjeta Citibanamex
-      </p>
-
-      <p className="text-2xl font-bold text-grayDark"> 
-        Celulares hasta 13 meses sin intereses
-      </p>
+     
     </>
   )
 }
