ef79754455a0dd0bcd41794f3207ea1e39362064
Ivonne <ivonne.scarlett01@gmail.com>
2022-09-30 10:47:07 -0500

    Conexión con la API RickAndMorty

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
-
+import React, { useState, useEffect } from 'react'
+import {getAllCharacters} from '../lib/api'
 
 export default function Home() {
+
+  const [characters, setCharacters] = useState([])
+   
+  useEffect(() => {
+    const characters = {}
+    getAllCharacters()
+    .then(response => {
+      setCharacters(response)
+    }) 
+  }, [])
+
+
   return (
     <>
-     
+     <div>
+        <div className="flex flex-wrap justify-evenly">   
+           
+          {
+            characters.map((item, index) => {
+              console.log('item',item)
+              return (   
+                <div key={index}>          
+                  <p>{item.id}</p>   
+                  <p>{item.name}</p>   
+                  <p>{item.species}</p>    
+                </div>
+              )
+            })
+          } 
+        </div>
+     </div>
     </>
   )
 }
