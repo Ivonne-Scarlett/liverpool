d0b9671ccfe8927c9ffff4fa6b994b5c1862e253
Ivonne <ivonne.scarlett01@gmail.com>
2022-10-02 17:48:50 -0500

    Funcionalidad del boton 'Salir' con Google Auth

--- components/ButtonSignUp.jsx
+++ components/ButtonSignUp.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Link from "next/link"
+import { initFirebase} from '../firebase/firebaseApp'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useRouter} from 'next/router'
 
 export default function ButtonSignUp() {
-  
+  const auth = getAuth()
+  const [user, loading] = useAuthState(auth)
+  const router = useRouter()
+
   return (
     <>
-      <Link href={"/login"}>
-        <button className="text-white">
+      <Link href={"/signGoogle"}>
+        <button className="text-white"
+         onClick={() => auth.signOut()}
+        >
           Salir
         </button>
       </Link>
